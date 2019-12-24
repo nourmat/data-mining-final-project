5ff31fb0948eaacbaddd60387d9fe96fea46051e
Nour E-Din <nourmanmatrix@gmail.com>
2019-12-24 15:06:03 +0200

    tweak

--- clustring.py
+++ clustring.py
@@ -5,13 +5,13 @@ KMEANS = 0
 class Clustering:
     def __init__(self,type=KMEANS, **kwargs):
         if type == KMEANS:
-            self.clustering= KMeans(**kwargs)
+            self.cluster = KMeans(**kwargs)
         
     def fit(self,X_train,y):
-        return self.classifier.fit(X_train,y)
+        return self.cluster.fit(X_train,y)
 
     def predict(self, X_test):
-        return self.classifier.predict(X_test)
+        return self.cluster.predict(X_test)
 
     def score(self, X_test, y_test):
-        return self.classifier.score(X_test, y_test)
+        return self.cluster.score(X_test, y_test)
